6b26f633d22ab829d1ba41f6bfa01ca6927d863f
CatsLover2006 <chancelorne@icloud.com>
2023-05-23 17:38:56 -0700

    First ver

---
+++ basemath.hpp
@@ -0,0 +1,23 @@
+namespace hailMath {
+
+	float abs(float x);
+
+	double abs(double x);
+
+	short abs(short x);
+
+	int abs(int x);
+
+	long abs(long x);
+
+	long long abs(long long x);
+
+	float constrain(float x, float min, float max);
+
+	float max (float a, float b);
+
+	float min (float a, float b);
+
+	extern float pi;
+
+}
